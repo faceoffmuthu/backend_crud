e9dfef50a9ce77af81e08babb02937f65b1a04ab
faceoffmuthu <faceoffmuthu@gmail.com>
2025-04-16 19:26:36 +0530

    connect to mongoDB with env file

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import express from "express"
 import moviesRoutes from "./routes/movies.route.js "
 import connectDB from "./lib/db.js";
 
+
 // connect DB should import the connectDB
 connectDB()
 
